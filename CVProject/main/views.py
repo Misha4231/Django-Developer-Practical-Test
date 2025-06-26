0aacb08d2d3ae758be6eeb042ef81b76c3148cd2
Misha4231 <mishagalko1@gmail.com>
2025-06-26 17:51:06 +0200

    cv views

--- CVProject/main/views.py
+++ CVProject/main/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpRequest
+from django.shortcuts import get_object_or_404
 
 from .models import CV
 
 def home(request: HttpRequest):
     cv_list = CV.objects.all()
     return render(request, 'home.html', {'cv_list': cv_list})
+
+def cv(request: HttpRequest, id: int):
+    cv = get_object_or_404(CV, pk=id)
+    return render(request, 'detail.html', {'cv': cv})
