46c9213b733632406a263f0f2ab753c18b65330f
Misha4231 <mishagalko1@gmail.com>
2025-06-26 23:32:03 +0200

    pdf generation

--- CVProject/main/urls.py
+++ CVProject/main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import home, cv
+from .views import home, cv, render_cv_pdf
 
 urlpatterns = [
     path('', home, name='home'),
-    path('cv/<int:id>/', cv, name='cv')
+    path('cv/<int:id>/', cv, name='cv'),
+    path('cv/<int:id>/pdf', render_cv_pdf, name='cv-pdf'),
 ]
--- CVProject/main/views.py
+++ CVProject/main/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404
+from weasyprint import HTML
+from django.template.loader import render_to_string
 
 from .models import CV
 
@@ -11,3 +13,16 @@ def home(request: HttpRequest):
 def cv(request: HttpRequest, id: int):
     cv = get_object_or_404(CV, pk=id)
     return render(request, 'detail.html', {'cv': cv})
+
+def render_cv_pdf(request: HttpRequest, id: int):
+    # Get html
+    cv = get_object_or_404(CV, pk=id)
+    html_string = render_to_string('detail.html', {'cv': cv, 'pdf': True})
+
+    # Assemble pdf response
+    pdf_file = HTML(string=html_string, base_url=request.build_absolute_uri()).write_pdf()
+
+    response = HttpResponse(pdf_file, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="CV.pdf"'
+
+    return response
