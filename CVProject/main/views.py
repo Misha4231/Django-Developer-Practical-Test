61206edb16463240af4b63072f2acadba323a0f6
Misha4231 <mishagalko1@gmail.com>
2025-06-26 17:32:07 +0200

    add home page

--- CVProject/main/views.py
+++ CVProject/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 
-# Create your views here.
+from .models import CV
+
+def home(request: HttpRequest):
+    cv_list = CV.objects.all()
+    return render(request, 'home.html', {'cv_list': cv_list})
